d7f93e2fe069ba1416ef1f97a2a87e06ce920708
João  Henrique Valente <47433650+jhvalente2023@users.noreply.github.com>
2023-07-31 07:59:03 +0000

    ejs instalado, set view engine inicial

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* IMPORTAÇÃO DAS BIBLIOTECAS EXPRESS, BCRYPT, PATH, CORS, EXPRESS-SESSION */
 const express = require("express");
 const app = express();
 const bcrypt = require("bcryptjs");
@@ -5,23 +6,32 @@ const path = require("path");
 const cors = require("cors");
 const session = require("express-session");
 
+/* CONFIGURAÇÕES INICIAIS DA SESSION, EXPRESS, CORS. SET DA VIEW ENGINE EJS */
+
+app.set("view engine", "ejs");
+
 app.use(
   session({
     secret: "CHRJFfs%#kpRu4VgeR8xN8pyKEpDaHrf#!9&8QTQA",
-    secure: true,
+    secure: false,
     resave: false,
     saveUninitialized: true,
     cookie: { maxAge: 60000000 },
   })
 );
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+/* ROTA PRINCIPAL (HOME PAGE, FUTURO INDEX.HTML (EJS)*/
+
 app.get("/", (req, res) => {
   res.send("Temporário Principal");
 });
+
+/* ABERTURA DO SERVIDOR NA PORTA 8080 */
 
 app.listen(8080, (req, res) => {
   console.log("Server Running");
