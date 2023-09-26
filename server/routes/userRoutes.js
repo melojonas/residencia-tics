55d391ee295ef9e82fb2e1d2deca7a73fee260ca
Jonas Melo <jonasmelo@ufrj.br>
2023-09-26 13:02:50 +0000

    orientações para o uso das rotas

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,11 +5,104 @@ const { isAuthorized, isAuthenticated } = require('../controllers/authController
 
 // Create and List Users
 
+// Manual para front-end @GuilhermeASousa @rodrigosouza974
+
+/**
+ * @api {post} /users Create a new user
+ * @apiName CreateUser
+ * @apiGroup User
+ * 
+ * @apiParam {String} [name] User's name.
+ * @apiParam {String} [email] User's email.
+ * @apiParam {String} [password] User's password.
+ * 
+ * @apiSuccess {Object} user User object.
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 201 CREATED
+ *     { message: 'Usuário cadastrado com sucesso!' }
+ */
+
+/**
+ * @api {get} /users Get all users
+ * @apiName ListUsers
+ * @apiGroup User
+ * 
+ * @apiSuccess {Object[]} users List of user objects.
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *       {
+ *         "name": "John Doe",
+ *         "email": "johndoe@example.com
+ *        },
+ *        {
+ *         "name": "Jane Doe",
+ *         "email": "janedoe@example.com
+ *         }
+ *     ]
+ */
+
 router.route('/')
     .post(isAuthenticated, usersController.createUser)
     .get(isAuthenticated, usersController.listUsers);
 
 // Read, Update and Delete Users by ID
+/**
+ * @api {put} /users/:user_id Update a user by ID
+ * @apiName UpdateUser
+ * @apiGroup User
+ * 
+ * @apiParam {String} user_id User's ID.
+ * 
+ * @apiSuccess {Object} user User object.
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "name": "John Doe",
+ *       "email": "johndoe@example.com",
+ *       "role": "discente",
+ *       "created": "2021-01-01T00:00:00.000Z",
+ *       "updated": "2021-01-01T00:00:00.000Z",
+ *     }
+ */
+
+/**
+ * @api {delete} /users/:user_id Delete a user by ID
+ * @apiName DeleteUser
+ * @apiGroup User
+ * 
+ * @apiParam {String} user_id User's ID.
+ * 
+ * @apiSuccess {Object} user User object.
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     { message: 'Usuário removido com sucesso!' }
+ */
+
+/**
+ * @api {get} /users/:user_id Get a user by ID
+ * @apiName GetUser
+ * @apiGroup User
+ * 
+ * @apiParam {String} [user_id] User's ID.
+ * 
+ * @apiSuccess {Object} user User object.
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "name": "John Doe",
+ *       "email": "johndoe@example.com",
+ *       "role": "discente",
+ *       "created": "2021-01-01T00:00:00.000Z",
+ *       "updated": "2021-01-01T00:00:00.000Z",
+ *     }
+ */
+
 router.route('/:user_id')
     .get(isAuthenticated, usersController.readUser)
     .put(isAuthenticated, usersController.updateUser)
