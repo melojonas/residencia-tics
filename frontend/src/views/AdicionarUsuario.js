b80fb98da1e33d043bdb9e109ed4a99110c1c654
GuilhermeASousa <113257053+GuilhermeASousa@users.noreply.github.com>
2023-10-27 14:44:04 +0000

    alterações no css

--- frontend/src/views/AdicionarUsuario.js
+++ frontend/src/views/AdicionarUsuario.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/App.css';
+import '../css/AdicionarUsuario.css';
 import Header from './partials/Header.js';
 import Sidebar from './partials/Sidebar.js';
 
@@ -8,7 +9,7 @@ function AdicionarUsuario() {
 
     const [nome, setNome] = useState('');
     const [matricula, setMatricula] = useState('');
-    const [funcao, setFuncao] = useState('discente');
+    const [funcao, setFuncao] = useState('');
     const [email, setEmail] = useState('');
     const [telefone, setTelefone] = useState('');
 
@@ -30,31 +31,32 @@ function AdicionarUsuario() {
             <div className="main">
                 <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
                 <main className="content">
-                    <h2>Adicionar Novo Usuário</h2>
+                    <h1>Adicionar Novo Usuário</h1>
                     <form onSubmit={handleSubmit}>
-                        <div>
+                        <div className='wrap-input'>
                             <label>Nome:</label>
-                            <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} required />
+                            <input className='input' type="text" value={nome} onChange={(e) => setNome(e.target.value)} required />
                         </div>
-                        <div>
+                        <div className='wrap-input'>
                             <label>Matrícula:</label>
-                            <input type="text" value={matricula} onChange={(e) => setMatricula(e.target.value)} required />
+                            <input className='input' type="text" value={matricula} onChange={(e) => setMatricula(e.target.value)} required />
                         </div>
-                        <div>
+                        <div className='wrap-input'>
                             <label>Função:</label>
                             <select value={funcao} onChange={(e) => setFuncao(e.target.value)} required>
+                                <option value="" disabled selected>Selecione:</option>
                                 <option value="discente">Discente</option>
                                 <option value="docente">Docente</option>
                                 <option value="funcionario">Funcionário</option>
                             </select>
                         </div>
-                        <div>
+                        <div className='wrap-input'>
                             <label>Email:</label>
-                            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                            <input className='input' type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                         </div>
-                        <div>
+                        <div className='wrap-input'>
                             <label>Telefone para Contato:</label>
-                            <input type="text" value={telefone} onChange={(e) => setTelefone(e.target.value)} />
+                            <input className='input' type="text" value={telefone} onChange={(e) => setTelefone(e.target.value)} />
                         </div>
                         <button type="submit">Adicionar Usuário</button>
                     </form>
