c45ef665a2c8aec763210f24d642376b6f15cea0
GuilhermeASousa <113257053+GuilhermeASousa@users.noreply.github.com>
2023-10-30 13:16:50 +0000

    adicionado menu suspenso

--- frontend/src/views/partials/Header.js
+++ frontend/src/views/partials/Header.js
@@ -1,25 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import AuthContext from '../../context/auth';
 import '../../css/Header.css';
 import logo from '../../img/perfil-logo.png';
 
-
 function Header({ toggleSidebar }) {
-
-    // Get user name from auth context
-    // const { user.name } = useContext(AuthContext);
     const { logout } = useContext(AuthContext);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    return ( 
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    return (
         <div className="topbar">
             <div className="mobile-menu-button">
                 <button id="menu-toggle" onClick={toggleSidebar}>☰</button>
             </div>
             <div className="topbar-content">
-                <button className="logout-button" onClick={logout}>Logout</button>
-                <img src={logo} alt="Logo" />
+                <div className="header-logo" onClick={toggleDropdown}>
+                    <img src={logo} alt="Logo" />
+                    {isDropdownOpen && (
+                        <div className="dropdown-menu">
+                            <button onClick={logout}>Logout</button>
+                            {/* Adicionar outras opções do menu aqui */}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
-} 
+}
+
 export default Header;
